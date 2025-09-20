df32778668c246bf1873ba621a9352727b8c029f
Eduardo24RC <22670085@itiguala.edu.mx>
2025-09-19 20:33:43 -0600

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/status')
 def status():
     return jsonify({
         "service": "demo-cloud",
-        "version": "1.0"
-        "timestamp":datetime.datetime.utcnow().isoformat() +"z"
+        "version": "1.0",
+        "timestamp": datetime.datetime.utcnow().isoformat() + "z"
     })
 
-    if _name_ == "_main_":
-        app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
